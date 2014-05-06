3f6853915bf5da92f64f56a6bacfad5bd9760db8
Sergio Azevedo <sergior.jr@gmail.com>
2014-05-06 20:22:45 -0300

    Refactor Rename HorarioConsulta para AgendamentoConsulta

--- db/schema.rb
+++ db/schema.rb
@@ -3,11 +3,12 @@ ActiveRecord::Schema.define do
     t.string :nome, null: false
   end
 
-  create_table :horarios_consulta do |t|
+  create_table :agendamento_consulta do |t|
     t.date :data, null: false
     t.time :hora_inicio, null: false
     t.time :hora_fim, null: false
+    t.references :paciente, index: {name: 'idx_agen_cons_pac'}
   end
-  add_index :horarios_consulta, :data, name: 'horario_consulta_por_data'
-  add_index :horarios_consulta, [:data, :hora_inicio, :hora_fim], name: 'horario_consulta_por_data_hora'
+  add_index :horarios_consulta, :data, name: 'idx_agen_data'
+  add_index :horarios_consulta, [:data, :hora_inicio, :hora_fim], name: 'idx_agen_data_hora'
 end
--- lib/clinica/infra/agendamento_consulta_data.rb
+++ lib/clinica/infra/agendamento_consulta_data.rb
@@ -1,7 +1,8 @@
-class HorarioConsultaData < ActiveRecord::Base
-  self.table_name = :horario_consulta
+class AgendamentoConsultaData < ActiveRecord::Base
+  self.table_name = :agendamento_consulta
 
   attr_accessible :data, :hora_inicio, :hora_fim
+  belongs_to :paciente
 
   validates :data, presense: true
   validates :hora_inicio, presense: true
