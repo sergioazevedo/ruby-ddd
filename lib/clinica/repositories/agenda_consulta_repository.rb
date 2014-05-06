65365b6e604bfb2a9a2480524634a3c83c196b0b
Sergio Azevedo <sergior.jr@gmail.com>
2014-05-06 19:35:50 -0300

    incrementa o agenda_consulta_repositorio.rb

--- lib/clinica/repositories/agenda_consulta_repository.rb
+++ lib/clinica/repositories/agenda_consulta_repository.rb
@@ -1,7 +1,8 @@
 class AgendaConsultaRepository
 
   def obter_agenda_do_dia(dia)
-
+    periodo = Periodo.do_dia_inteiro( dia )
+    AgendaConsulta.new(periodo: periodo, repositorio: self)
   end
 
 end
--- spec/clinica/unit/repositories/agenda_consulta_repository_spec.rb
+++ spec/clinica/unit/repositories/agenda_consulta_repository_spec.rb
@@ -3,12 +3,14 @@ require 'clinica/repositories/agenda_consulta_repository'
 
 describe 'Repositorio de Agenda de Consultas' do
 
-  describe "#initialize" do
-
-  end
+  Given(:repositorio){ AgendaConsultaRepository.new }
 
   describe "#obter_agenda_do_dia" do
-    pending "reason"
+    context "deve retornar a agenda de consultas para o dia em questao" do
+      Given(:data){ Time.local(2014, 6, 6, 9, 30) }
+      When(:result){ repositorio.obter_agenda_do_dia(data) }
+      Then{ expect(result).to be_an_instance_of(AgendaConsulta) }
+    end
   end
 
 end
