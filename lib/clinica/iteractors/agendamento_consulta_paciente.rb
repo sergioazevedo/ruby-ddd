a278e2e8fa8600b1be6599f62ebafecf6cff67fb
Sergio Azevedo <sergior.jr@gmail.com>
2014-05-06 14:18:04 -0300

    refator para usar Periodo

--- lib/clinica/iteractors/agendamento_consulta_paciente.rb
+++ lib/clinica/iteractors/agendamento_consulta_paciente.rb
@@ -2,17 +2,16 @@ class AgendamentoConsultaPaciente
 
   def initialize(paciente_id:, data_hora_inicio_consulta:, data_hora_fim_consulta:, opts: {})
     @paciente_id = paciente_id
-    @data_hora_inicio_consulta = data_hora_inicio_consulta
-    @data_hora_fim_consulta = data_hora_fim_consulta
+    @periodo_consulta = Periodo.new(inicio: data_hora_inicio_consulta, fim: data_hora_fim_consulta)
     @agenda_consulta_repositorio = opts[:agenda_consulta_repositorio] || AgendaConsultaRepositorio.new
     @paciente_repositorio = opts[:paciente_repositorio] || PacienteRepositorio.new
   end
 
   def execute
     paciente = @paciente_repositorio.obter_por_id(@paciente_id)
-    agenda_consulta = @agenda_consulta_repositorio.obter_agenda_do_dia(@data_hora_inicio_consulta)
+    agenda_consulta = @agenda_consulta_repositorio.obter_agenda_do_dia( @periodo_consulta.data_inicio )
 
-    agendamento_consulta = agenda_consulta.agendar_horario(paciente: paciente, inicio: @data_hora_inicio_consulta, fim: @data_hora_fim_consulta)
+    agendamento_consulta = agenda_consulta.agendar_horario(paciente: paciente, periodo: @periodo_consulta)
 
     agendamento_consulta
   end
