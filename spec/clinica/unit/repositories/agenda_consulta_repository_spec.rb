e86c1738b20b133d7c778f7ed4557eaa5d5ff33a
Sergio Azevedo <sergior.jr@gmail.com>
2014-05-06 20:23:57 -0300

    melhora a spec de AgendaConsultaRepository adicionando:  - teste de contarto de API  - spec para comportamento de relizar agendamento  - spec para comportamento de período ja agendado

--- spec/clinica/unit/repositories/agenda_consulta_repository_spec.rb
+++ spec/clinica/unit/repositories/agenda_consulta_repository_spec.rb
@@ -5,12 +5,26 @@ describe 'Repositorio de Agenda de Consultas' do
 
   Given(:repositorio){ AgendaConsultaRepository.new }
 
+  describe "API" do
+    Then{ expect(repositorio).to respond_to(:obter_agenda_do_dia) }
+    Then{ expect(repositorio).to respond_to(:realizar_agendamento) }
+    Then{ expect(repositorio).to respond_to(:periodo_ja_agendado?) }
+  end
+
   describe "#obter_agenda_do_dia" do
     context "deve retornar a agenda de consultas para o dia em questao" do
       Given(:data){ Time.local(2014, 6, 6, 9, 30) }
       When(:result){ repositorio.obter_agenda_do_dia(data) }
       Then{ expect(result).to be_an_instance_of(AgendaConsulta) }
     end
+  end
+
+  describe "#realizar_agendamento" do
+    pending "reason"
+  end
+
+  describe "#periodo_ja_agendado?" do
+    pending "reason"
   end
 
 end
