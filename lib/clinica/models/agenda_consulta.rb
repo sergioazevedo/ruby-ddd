0f39cb9cca05f953864cea97f5338fbd5bf235d0
Sergio Azevedo <sergior.jr@gmail.com>
2014-05-06 19:35:36 -0300

    incrementa o agenda_consulta.rb

--- lib/clinica/models/agenda_consulta.rb
+++ lib/clinica/models/agenda_consulta.rb
@@ -1,6 +1,12 @@
 class AgendaConsulta
 
   def initialize(periodo:, repositorio:)
-
+    @periodo = periodo
+    @repositorio = repositorio
   end
+
+  def periodo
+    @periodo.freeze
+  end
+
 end
--- spec/clinica/unit/models/agenda_consulta_spec.rb
+++ spec/clinica/unit/models/agenda_consulta_spec.rb
@@ -14,19 +14,38 @@ describe 'Agenda de Consulta' do
 
       context "recebendo um periodo valido, mas sem referencia de repositorio" do
         When(:agenda){ AgendaConsulta.new(periodo: periodo_valido) }
-        Then{ expect(agenda).to raise_error(ArgumentError) }
+        Then{ expect(agenda).to raise_error }
       end
 
       context "recebendo um periodo valido e com referencia de repositorio" do
         When(:agenda){ AgendaConsulta.new(periodo: periodo_valido, repositorio: repositorio) }
-        Then{ expect(agenda).to_not raise_error(ArgumentError) }
+        Then{ expect{ agenda }.to_not raise_error }
       end
 
     end
   end
 
-  describe "#agendar_horario" do
-    pending "reason"
+  describe "API" do
+    let(:periodo_valido){ double }
+    let(:repositorio){ double }
+
+    Given(:agenda){ AgendaConsulta.new(periodo: periodo_valido, repositorio: repositorio) }
+
+    context "deve responder ao periodo" do
+      Then{ expect(agenda).to respond_to(:periodo) }
+    end
+    context "nao deve permitir alteracoes no periodo" do
+      Then{ expect(agenda).to_not respond_to(:periodo=) }
+    end
+
+    context "deve responder ao metodo agendar_horario" do
+      Then{ expect(agenda).to_not respond_to(:agendar_horario) }
+    end
   end
+
+  describe "#agendar_horario" do
+
+  end
+
 
 end
