274b403ecd36bdfe3ba92d2a717f7839b4c605b9
Sergio Azevedo <sergior.jr@gmail.com>
2014-05-09 14:09:21 -0300

    refatora o schema

--- db/schema.rb
+++ db/schema.rb
@@ -4,11 +4,10 @@ ActiveRecord::Schema.define do
   end
 
   create_table :agendamento_consulta do |t|
-    t.date :data, null: false
-    t.time :hora_inicio, null: false
-    t.time :hora_fim, null: false
+    t.datetime :data_hora_inicio, null: false
+    t.datetime :data_hora_fim, null: false
     t.references :paciente, index: {name: 'idx_agen_cons_pac'}
   end
-  add_index :horarios_consulta, :data, name: 'idx_agen_data'
-  add_index :horarios_consulta, [:data, :hora_inicio, :hora_fim], name: 'idx_agen_data_hora'
+  add_index :horarios_consulta, [:data_hora_inicio, :data_hora_fim], name: 'idx_agen_data_hora'
+  add_index :horarios_consulta, [:paciente, :data_hora_inicio, :data_hora_fim], name: 'idx_agen_paci_data_hora'
 end
