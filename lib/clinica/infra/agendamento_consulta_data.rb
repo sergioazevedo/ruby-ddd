99c582383cbcf7a42afeaf39364fc73ae2fe2282
Sergio Azevedo <sergior.jr@gmail.com>
2014-05-09 14:10:01 -0300

    refatora os atributos de AgendamentoConsultaData e inlcui o metodo horario_disponivel_para_agendamento?

--- lib/clinica/infra/agendamento_consulta_data.rb
+++ lib/clinica/infra/agendamento_consulta_data.rb
@@ -1,10 +1,14 @@
 class AgendamentoConsultaData < ActiveRecord::Base
   self.table_name = :agendamento_consulta
 
-  attr_accessible :data, :hora_inicio, :hora_fim
-  belongs_to :paciente
+  attr_accessible :data_hora_inicio, :data_hora_fim
+  belongs_to :paciente, class_name: :PacienteData
 
-  validates :data, presense: true
-  validates :hora_inicio, presense: true
-  validates :hora_fim, presense: true
+  validates :data_hora_inicio, presense: true
+  validates :data_hora_fim, presense: true
+
+  def self.horario_disponivel_para_agendamneto?(data_hora_inicio, data_hora_fim)
+    where( data_hora_inicio: data_hora_inicio...data_hora_fim,
+           data_hora_fim: data_hora_fim..data_hora_inicio )
+  end
 end
