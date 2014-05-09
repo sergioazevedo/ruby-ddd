40e9e605458e9b6c0ef83d37de5efe3b080b4a94
Sergio Azevedo <sergior.jr@gmail.com>
2014-05-09 15:36:17 -0300

    remove as validates do active_model temporariamente

--- lib/clinica/infra/agendamento_consulta_data.rb
+++ lib/clinica/infra/agendamento_consulta_data.rb
@@ -3,10 +3,8 @@ class AgendamentoConsultaData < ActiveRecord::Base
   self.table_name = :agendamento_consulta
   belongs_to :paciente, class_name: :PacienteData
 
-  attr_accessible :paciente, :data_hora_inicio, :data_hora_fim
-
-  validates :data_hora_inicio, presense: true
-  validates :data_hora_fim, presense: true
+  # validates :data_hora_inicio, presense: true
+  # validates :data_hora_fim, presense: true
 
   def self.horario_disponivel_para_agendamneto?(data_hora_inicio, data_hora_fim)
     where( data_hora_inicio: data_hora_inicio...data_hora_fim,
