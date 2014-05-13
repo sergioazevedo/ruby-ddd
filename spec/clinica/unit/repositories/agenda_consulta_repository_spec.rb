6028a2830470a059cae2a45068117575872c0bb2
Sergio Azevedo <sergior.jr@gmail.com>
2014-05-13 13:16:58 -0300

    adiciona spec para #periodo_diponivel_para_agendamento?

--- spec/clinica/unit/repositories/agenda_consulta_repository_spec.rb
+++ spec/clinica/unit/repositories/agenda_consulta_repository_spec.rb
@@ -8,7 +8,7 @@ describe 'Repositorio de Agenda de Consultas' do
   describe "API" do
     Then{ expect(repositorio).to respond_to(:obter_agenda_do_dia) }
     Then{ expect(repositorio).to respond_to(:realizar_agendamento) }
-    Then{ expect(repositorio).to respond_to(:periodo_disponivel_para_agendamneto?) }
+    Then{ expect(repositorio).to respond_to(:periodo_disponivel_para_agendamento?) }
   end
 
   describe "#obter_agenda_do_dia" do
@@ -19,12 +19,30 @@ describe 'Repositorio de Agenda de Consultas' do
     end
   end
 
-  describe "#realizar_agendamento" do
-    pending "reason"
+  describe "#periodo_disponivel_para_agendamento?" do
+    let(:data_inicio){ Time.local(2014, 6, 6, 9, 30) }
+    let(:data_fim){ Time.local(2014, 6, 6, 10, 0) }
+    let(:periodo){ Periodo.new(inicio: data_inicio, fim: data_fim)}
+
+    context "com horario disponivel" do
+      Given(:data_object){ double("dao", horario_disponivel_para_agendamento?: true) }
+      Given(:repositorio){ AgendaConsultaRepository.new(data_object: data_object) }
+      When(:result) { repositorio.periodo_disponivel_para_agendamento?(periodo) }
+      Then{ expect(result).to be_true }
+    end
+
+    context "com horario indisponivel" do
+      Given(:data_object){ double("dao", horario_disponivel_para_agendamento?: false) }
+      Given(:repositorio){ AgendaConsultaRepository.new(data_object: data_object) }
+      When(:result) { repositorio.periodo_disponivel_para_agendamento?(periodo) }
+      Then{ expect(result).to be_false }
+    end
   end
 
-  describe "#periodo_disponivel_para_agendamneto?" do
-    pending "reason"
+  describe "#realizar_agendamento" do
+    let(:dao){ double }
+    Given(:repositorio){ AgendaConsultaRepository.new(data_object: dao) }
+    pending
   end
 
 end
