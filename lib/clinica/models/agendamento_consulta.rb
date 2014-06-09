a9c352b607aefce7c63852d24740c37e5d8f7d4c
Sergio Azevedo <sergior.jr@gmail.com>
2014-06-09 09:50:18 -0300

    incrementa o AgendamentoConsulta

--- lib/clinica/models/agendamento_consulta.rb
+++ lib/clinica/models/agendamento_consulta.rb
@@ -1,6 +1,18 @@
+require 'forwardable.rb'
 class AgendamentoConsulta
+  extend Forwardable
+
+  def_delegator :@data_instance, :id, :paciente_id
+  def_delegator :@data_instance, :nome, :paciente_nome
 
   def initialize(data_instance)
+    @data_instance = data_instance
+  end
+
+  def periodo
+    @periodo ||= Periodo.new( inicio: data_instance.data_hora_inicio, fim: data_instance.data_hora_fim )
+
+    @periodo.freeze
   end
 
 end
--- spec/clinica/unit/models/agendamento_consulta_spec.rb
+++ spec/clinica/unit/models/agendamento_consulta_spec.rb
@@ -1,16 +1,42 @@
+# encoding: utf-8
+
 require 'db_helper'
 require 'rspec/given'
 require 'clinica/models/agendamento_consulta'
 require 'clinica/infra/agendamento_consulta_data'
 
 describe 'Agendamento Consulta' do
+  describe "API" do
+    let(:data_instance){ double }
+    subject(:agendamento){ AgendamentoConsulta.new(data_instance) }
+
+    it "deve responder ao método #periodo" do
+      expect(agendamento).to respond_to(:periodo)
+    end
+
+    it "deve ser capaz de responder ao nome do paciente" do
+      expect(agendamento).to respond_to(:paciente_nome)
+    end
+
+    it "deve ser capaz de responder ao id do paciente" do
+      expect(agendamento).to respond_to(:paciente_id)
+    end
+  end
 
   describe "#initialize" do
     context "sem receber um data_instance deve lancar excecao" do
       When(:agendamento_consulta){ AgendamentoConsulta.new }
       Then{ expect(agendamento_consulta).to raise_error }
     end
+  end
 
+  describe "Relacionamento com Paciente" do
+    let(:data_instance){ double(:dao, nome: 'Ricardo') }
+    subject(:agendamento){ AgendamentoConsulta.new(data_instance) }
+
+    it "deve retornar um valor nao nulo para nome do paciente" do
+      expect(agendamento.paciente_nome).to_not be nil
+    end
   end
 
 end
